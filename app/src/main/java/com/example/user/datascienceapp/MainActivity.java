902924ae4fe0c33ad1e288b204734b0c6bbfa037
lakshay1701 <lakshaygupta1701@gmail.com>
2016-08-08 17:40:32 +0530

    orange color theme

--- app/src/main/java/com/example/user/datascienceapp/MainActivity.java
+++ app/src/main/java/com/example/user/datascienceapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.user.datascienceapp;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TypefaceSpan;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
