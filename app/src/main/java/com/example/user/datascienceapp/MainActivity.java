e33e34e7db5c25a1ddd3b5945a9deed9d5164ea3
lakshay1701 <lakshaygupta1701@gmail.com>
2016-08-09 23:00:36 +0530

    slider layout

--- app/src/main/java/com/example/user/datascienceapp/MainActivity.java
+++ app/src/main/java/com/example/user/datascienceapp/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.user.datascienceapp;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.TypefaceSpan;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+   Button story;
+//    static  int count=0;
+   // int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        story=(Button)findViewById(R.id.story_button);
+        story.setOnClickListener(this);
 
 
     }
+public void onClick(View w)
+{
+    switch(w.getId())
+    {
+        case R.id.story_button:
+            Intent next=new Intent(MainActivity.this,Slider_activity.class);
+            startActivity(next);
+            break;
+
+    }
+
+
+}
 
 
 }
